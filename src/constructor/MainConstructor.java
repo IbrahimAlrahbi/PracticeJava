8b32d2111bedede19381368c7d5dc867579ba665
ibrahbi <ibra7bi@gmail.com>
2026-04-19 17:28:30 +0400

    Addition

--- src/constructor/MainConstructor.java
+++ src/constructor/MainConstructor.java
@@ -6,7 +6,7 @@ public class MainConstructor {
         Product p1 = new Product("Camera", "Auto focus...", 99,10);
         Product p2 = new Product(); //  it will match and read, print from it
 
-        //  Sometimes I need to copy from p1 - Copy Constructors
+        //  Sometimes I need to copy from p1 - "Copy Constructors-Type"
         Product p3 = new Product(p1);
         p1.display();
         p2.display();
