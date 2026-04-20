eb465547dcdc26315661238743faa19fea57a647
ibrahbi <ibra7bi@gmail.com>
2026-04-20 16:18:25 +0400

    Enhancements

--- src/understandStatic/MainStudent.java
+++ src/understandStatic/MainStudent.java
@@ -1,7 +1,10 @@
 package understandStatic;
 
 public class MainStudent {
+
     public static void main(String[] args){
         Student student1 = new Student(123,"Ali");
+        //  static is not owned or reserved by anyone and can be called using class
+        System.out.println(Student.college);
     }
 }
